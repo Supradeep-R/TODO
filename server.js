e19096ceea21bc689087611f7866ffe3f9133797
Supradeep-R <revuru.supradeep@gmail.com>
2024-03-17 18:43:41 +0530

    rolled back to previous version by changing code

--- server.js
+++ server.js
@@ -8,18 +8,6 @@ const mongoDB = require("./db");
 const UserRoutes = require("./routes/UserRoutes");
 const TodoRoutes = require("./routes/TodoRoutes");
 mongoDB();
-const path = require("path");
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-
-// Your API routes here
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
 
 app.get("/", (req, res) => {
   app.use(express.static(path.resolve(__dirname, "client", "build")));
